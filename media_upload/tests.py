77e1a95bbfb070cbeffe782ddb52c0669844adde
JuanCarlos <j.c.chacon@outlook.com>
2020-10-13 18:52:14 +0200

    added tests

--- media_upload/tests.py
+++ media_upload/tests.py
@@ -1,3 +1,25 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
+from django.urls import resolve, reverse
 
-# Create your tests here.
+from .views import upload
+
+
+class MediaUploadTests(TestCase):
+    def test_upload_view_status_code(self):
+        url = reverse('upload')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_upload_url_resolves_upload_view(self):
+        view = resolve('/')
+        self.assertEqual(view.func, upload)
+
+    def test_upload_file(self):
+        url = reverse('upload')
+        image = SimpleUploadedFile('file.jpg', b'file_content', content_type='multipart/form-data')
+        response = self.client.post(url, {'uploaded_file': image})
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('file_url', response.context)
+        self.assertContains(response, 'File uploaded at:')
--- media_upload/views.py
+++ media_upload/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 
 
-def image_upload(request):
-    if request.method == 'POST' and request.FILES['image_file']:
-        image_file = request.FILES['image_file']
+def upload(request):
+    if request.method == 'POST' and request.FILES['uploaded_file']:
+        uploaded_file = request.FILES['uploaded_file']
         fs = FileSystemStorage()
-        filename = fs.save(image_file.name, image_file)
-        image_url = fs.url(filename)
+        filename = fs.save(uploaded_file.name, uploaded_file)
+        file_url = fs.url(filename)
         return render(request, 'upload.html', {
-            'image_url': image_url,
+            'file_url': file_url,
         })
     return render(request, 'upload.html')
